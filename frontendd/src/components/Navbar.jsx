6a996a918493f810e8c97dceea68412f75ee1ead
Abhishek <abhishekofficials12@gmail.com>
2026-01-27 16:20:32 +0530

    --

--- frontendd/src/components/Navbar.jsx
+++ frontendd/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import axios from '../utils/axiosConfig'
 import './Navbar.css'
 import StoryUploader from './StoryUploader'
 import NotificationCenter from './NotificationCenter'
--- frontendd/src/pages/Profile.jsx
+++ frontendd/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import axios from '../utils/axiosConfig'
 import PostThumbnail from '../components/PostThumbnail'
 import './Profile.css'
 
